1bcd2c67165ff425c4f1fe89eecdc7dd60af27bd
Arkadiusz Plumbaum <arekpl28@wp.pl>
2022-08-16 11:25:07 +0200

    JS add element

---
+++ a16JavaScript/e3.js
@@ -0,0 +1,6 @@
+const btnAddSquare = document.querySelector("button");
+
+btnAddSquare.addEventListener("click", () => {
+  const div = document.createElement("div");
+  document.body.appendChild(div);
+});
